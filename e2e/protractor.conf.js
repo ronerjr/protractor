f86c4dba39b8ed407de1632fa15a6bfd6c22cdcb
Roner Damaso Junior <ronerjr@hotmail.com>
2018-07-29 20:51:44 -0300

    fifth version of tests - allure

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -24,5 +24,17 @@ exports.config = {
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    const AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+    jasmine.getEnv().afterEach(function(done){
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64')
+        }, 'image/png')();
+        done();
+      })
+    });
   }
 };
